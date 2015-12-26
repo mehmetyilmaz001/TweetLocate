d942ceeec7c713c7dd7225e89196eac7931d8474
Mehmet Yilmaz <mehmetyilmaz001@gmail.com>
2015-12-26 10:57:31 +0200

    Initial Commit

---
+++ TweetLocate/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TweetLocate
+//
+//  Created by Mehmet Yilmaz on 26/12/15.
+//  Copyright © 2015 Ninjakod. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
